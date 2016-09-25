499397d6bc8f2810cc527ca6d41ed8ee77a7c656
mchan615 <mchan615@gmail.com>
2016-09-24 22:18:53 -0400

    added score, part, phrase

--- src/Main.java
+++ src/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
     public static void main(String[] args) {
         //MusicGenerator gen = new MusicGenerator(dummyScore());
         //gen.play();
+        System.out.println(JMC.MAJOR_SCALE.length);
         launch(args);
     }
 
